4f67e3f9014250801af4c6fa4baf002b62d8ed6c
Jesse A. Tov <jesse.tov@gmail.com>
2017-07-18 12:42:50 -0500

    Fixed deps?

--- info.rkt
+++ info.rkt
@@ -4,8 +4,8 @@
 (define can-be-loaded-with 'all)
 (define required-core-version "6.9")
 (define version "2.2")
-(define deps '("base" "rackunit"))
-(define build-deps '("scribble-lib" "racket-doc" "at-exp-lib" "sandbox-lib" "compatibility-doc"))
+(define deps '("base" "rackunit-lib" "parser-tools-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/dssl2.scrbl" ())))
 (define blurb
     '("A language for data structures students, version 2"))

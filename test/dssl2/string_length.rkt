06a9170737bcaa2cb240f43e9bbc89adf8f5f690
Jesse A. Tov <jesse.tov@gmail.com>
2017-07-25 17:00:56 -0500

    Updated test for strlen.

--- test/dssl2/string_length.rkt
+++
@@ -1,5 +0,0 @@
-#lang dssl2
-
-assert_eq string_length(''), 0
-assert_eq string_length('0'), 1
-assert_eq string_length('01234'), 5
---
+++ test/dssl2/strlen.rkt
@@ -0,0 +1,5 @@
+#lang dssl2
+
+assert_eq strlen(''), 0
+assert_eq strlen('0'), 1
+assert_eq strlen('01234'), 5

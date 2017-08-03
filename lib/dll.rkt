4b65729bbc09f33f044d09bd1f71d64489b5b941
Jesse A. Tov <jesse.tov@gmail.com>
2017-08-03 13:59:28 -0500

    Contracted Dll lib; test

--- lib/dll.rkt
+++ lib/dll.rkt
@@ -1,147 +1,17 @@
 #lang dssl2
 
-# A DllOf<X> is an object with the methods listed below.
+def MakeDll(X: contract?):
+    def MaybeC(X): OrC(X, False)
+    defstruct Node(
+       prev: MaybeC(Node?), # Only includes False for initialization
+       data: MaybeC(X),     # Only includes False for sentinel
+       next: MaybeC(Node?)) # Only includes False for initialization
 
-# Dll_from_sentinel_and_size : NodeOf<X> Natural -> DllOf<X>
-def Dll_from_sentinel_and_size(sentinel_, size_):
-    # A NodeOf<X> is
-    #   Node(NodeOf<X>, X or False
-    defstruct Node(prev, data, next)
-
-    if sentinel_ === False:
-        sentinel_ = Node(False, False, False)
-        sentinel_.prev = sentinel_
-        sentinel_.next = sentinel_
-
-    # : Self -> Boolean
-    def empty?():
-        sentinel_.next === sentinel_
-
-    # : Self -> Natural
-    def size():
-        size_
-
-    # : Self -> NodeOf<X>
-    def get_sentinel!(): sentinel_
-
-    # : Self NodeOf<X> Natural -> Void
-    def set_sentinel_and_size!(sentinel, size):
-        sentinel_ = sentinel
-        size_ = size
-
-    # : Self DllOf<X> -> Void
-    def swap(other):
-        let sentinel = other.get_sentinel!()
-        let size = other.size()
-        other.set_sentinel_and_size!(sentinel_, size_)
-        set_sentinel_and_size!(sentinel, size)
-
-    # : Self NodeOf<X> NodeOf<X> Natural -> DllOf<X>
-    # Precondition: count is number of nodes in [start, limit)
-    def detach(start, limit, count):
-        if start === limit: return Dll()
-        let new_sentinel = Node(limit.prev, False, start)
-        limit.prev = start.prev
-        limit.prev.next = limit
-        new_sentinel.next.prev = new_sentinel
-        new_sentinel.prev.next = new_sentinel
-        size_ = size_ - count
-        Dll_from_sentinel_and_size(new_sentinel, count)
-
-    # : Self Natural -> DllOf<X>
-    def detach_front(n):
-        let count = 0
-        let limit = sentinel_
-        while limit.next !== sentinel_ and count < n:
-            limit = limit.next
-            count = count + 1
-        detach(sentinel_.next, limit.next, count)
-
-    # : Self Natural -> DllOf<X>
-    def detach_back(n):
-        let count = 0
-        let start = sentinel_
-        while start.prev !== sentinel_ and count < n:
-            start = start.prev
-            count = count + 1
-        detach(start, sentinel_, count)
-
-    # : Self DllOf<X> -> Void
-    def splice(other):
-        let other_sentinel = other.get_sentinel!()
-        sentinel_.prev.next = other_sentinel.next
-        other_sentinel.next.prev = sentinel_.prev
-        sentinel_.prev = other_sentinel.prev
-        sentinel_.prev.next = sentinel_
-        size_ = size_ + other.size()
-        other_sentinel.next = other_sentinel
-        other.sentinel.prev = other_sentinel
-        other.set_sentinel_and_size!(other_sentinel, 0)
-
-    # : Self X -> Void
-    def push_front(value):
-        let new_node = Node(sentinel_, value, sentinel_.next)
-        new_node.next.prev = new_node
-        new_node.prev.next = new_node
-        size_ = size_ + 1
-
-    # : Self X -> Void
-    def push_back(value):
-        let new_node = Node(sentinel_.prev, value, sentinel_)
-        new_node.next.prev = new_node
-        new_node.prev.next = new_node
-        size_ = size_ + 1
-
-    # : Self -> X or False
-    def pop_front():
-        if sentinel_.next == sentinel_: return False
-        let result = sentinel_.next.data
-        sentinel_.next = sentinel_.next.next
-        sentinel_.next.prev = sentinel_
-        result
-
-    # : Self -> X or False
-    def pop_back():
-        if sentinel_.prev == sentinel_: return False
-        let result = sentinel_.prev.data
-        sentinel_.prev = sentinel_.prev.prev
-        sentinel_.prev.next = sentinel_
-        result
-
-    # : Self [Y X -> Y] Y -> Y
-    def foldl(f, z):
-        let current = sentinel_.next
-        while current !== sentinel_:
-            z = f(z, current.data)
-            current = current.next
-        return z
-
-    # : Self [X Y -> Y] Y -> Y
-    def foldr(f, z):
-        let current = sentinel_.prev
-        while current !== sentinel_:
-            z = f(current.data, z)
-            current = current.prev
-        return z
-
-    # : Self [Natural X -> Void] -> Void
-    def each_with_index(f):
-        def each(i, x):
-            f(i, x)
-            i + 1
-        foldl(each, 0)
-        pass
-
-    # : Self -> VectorOf<X>
-    def to_vector():
-        let v = [False; size()]
-        def each(i, x): v[i] = x
-        each_with_index(each)
-        v
-
-    object Dll {
+    defstruct Dll (
         empty?,
         size,
+        front,
+        back,
         push_front,
         push_back,
         pop_front,
@@ -155,8 +25,162 @@ def Dll_from_sentinel_and_size(sentinel_, size_):
         to_vector,
         get_sentinel!,
         set_sentinel_and_size!,
-    }
+    )
 
-def Dll():
-    Dll_from_sentinel_and_size(False, 0)
+    def from_sentinel_and_size(sentinel_: MaybeC(Node?), size_: int?) -> Dll?:
+        if sentinel_ === False:
+            sentinel_ = Node(False, False, False)
+            sentinel_.prev = sentinel_
+            sentinel_.next = sentinel_
+
+        def empty?() -> bool?:
+            sentinel_.next === sentinel_
+
+        def size() -> int?:
+            size_
+
+        def get_sentinel!() -> Node?: sentinel_
+
+        def set_sentinel_and_size!(sentinel: Node?, size: int?):
+            sentinel_ = sentinel
+            size_ = size
+
+        def swap(other: Dll?):
+            let sentinel = other.get_sentinel!()
+            let size = other.size()
+            other.set_sentinel_and_size!(sentinel_, size_)
+            set_sentinel_and_size!(sentinel, size)
+
+        # Precondition: count is number of nodes in [start, limit)
+        def detach(start: Node?, limit: Node?, count: int?) -> Dll?:
+            if start === limit: return empty()
+            let new_sentinel = Node(limit.prev, False, start)
+            limit.prev = start.prev
+            limit.prev.next = limit
+            new_sentinel.next.prev = new_sentinel
+            new_sentinel.prev.next = new_sentinel
+            size_ = size_ - count
+            from_sentinel_and_size(new_sentinel, count)
+
+        def detach_front(n: int?) -> Dll?:
+            let count = 0
+            let limit = sentinel_
+            while limit.next !== sentinel_ and count < n:
+                limit = limit.next
+                count = count + 1
+            detach(sentinel_.next, limit.next, count)
+
+        def detach_back(n: int?) -> Dll?:
+            let count = 0
+            let start = sentinel_
+            while start.prev !== sentinel_ and count < n:
+                start = start.prev
+                count = count + 1
+            detach(start, sentinel_, count)
+
+        def splice(other: Dll?):
+            let other_sentinel = other.get_sentinel!()
+            sentinel_.prev.next = other_sentinel.next
+            other_sentinel.next.prev = sentinel_.prev
+            sentinel_.prev = other_sentinel.prev
+            sentinel_.prev.next = sentinel_
+            size_ = size_ + other.size()
+            other_sentinel.next = other_sentinel
+            other.sentinel.prev = other_sentinel
+            other.set_sentinel_and_size!(other_sentinel, 0)
+
+        def push_front(value: X):
+            let new_node = Node(sentinel_, value, sentinel_.next)
+            new_node.next.prev = new_node
+            new_node.prev.next = new_node
+            size_ = size_ + 1
+
+        def push_back(value: X):
+            let new_node = Node(sentinel_.prev, value, sentinel_)
+            new_node.next.prev = new_node
+            new_node.prev.next = new_node
+            size_ = size_ + 1
+
+        def pop_front() -> MaybeC(X):
+            if sentinel_.next === sentinel_: return False
+            let result = sentinel_.next.data
+            sentinel_.next = sentinel_.next.next
+            sentinel_.next.prev = sentinel_
+            size_ = size_ - 1
+            result
+
+        def pop_back() -> MaybeC(X):
+            if sentinel_.prev === sentinel_: return False
+            let result = sentinel_.prev.data
+            sentinel_.prev = sentinel_.prev.prev
+            sentinel_.prev.next = sentinel_
+            size_ = size_ - 1
+            result
+
+        def front() -> MaybeC(X):
+            if sentinel_.next === sentinel_: False
+            else: sentinel_.next.data
+
+        def back() -> MaybeC(X):
+            if sentinel_.prev === sentinel_: False
+            else: sentinel_.prev.data
+
+        # : Self [Y X -> Y] Y -> Y
+        def foldl(f: FunC(AnyC, X, AnyC), z):
+            let current = sentinel_.next
+            while current !== sentinel_:
+                z = f(z, current.data)
+                current = current.next
+            return z
+
+        # : Self [X Y -> Y] Y -> Y
+        def foldr(f: FunC(X, AnyC, AnyC), z):
+            let current = sentinel_.prev
+            while current !== sentinel_:
+                z = f(current.data, z)
+                current = current.prev
+            return z
+
+        # : Self [Natural X -> Void] -> Void
+        def each_with_index(f: FunC(int?, X, AnyC)):
+            def each(i, x):
+                f(i, x)
+                i + 1
+            foldl(each, 0)
+            pass
+
+        # : Self -> VectorOf<X>
+        def to_vector() -> vec?:
+            let v = [False; size()]
+            def each(i, x): v[i] = x
+            each_with_index(each)
+            v
+
+        Dll {
+            empty?,
+            size,
+            front,
+            back,
+            push_front,
+            push_back,
+            pop_front,
+            pop_back,
+            detach_front,
+            detach_back,
+            splice,
+            foldl,
+            foldr,
+            each_with_index,
+            to_vector,
+            get_sentinel!,
+            set_sentinel_and_size!,
+        }
+
+    def empty() -> Dll?:
+        from_sentinel_and_size(False, 0)
+
+    object DllFactory {
+        from_sentinel_and_size,
+        empty,
+    }
 
---
+++ test/dssl2/dll.rkt
@@ -0,0 +1,21 @@
+#lang dssl2
+
+import dll
+
+let IntDll = MakeDll(int?)
+
+let lst = IntDll.empty()
+
+lst.push_back(3)
+lst.push_back(4)
+
+assert_eq lst.front(), 3
+assert_eq lst.back(), 4
+
+assert_eq lst.size(), 2
+
+assert_eq lst.pop_front(), 3
+assert_eq lst.pop_front(), 4
+assert_eq lst.pop_front(), False
+
+assert_eq lst.size(), 0

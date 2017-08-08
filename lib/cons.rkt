1d06b3e448b997332148050008388b0b0291f976
Jesse A. Tov <jesse.tov@gmail.com>
2017-08-08 08:20:17 -0500

    Purpose statements in lib/cons

--- lib/cons.rkt
+++ lib/cons.rkt
@@ -3,23 +3,32 @@
 defstruct nil()
 defstruct cons(car: AnyC, cdr: OrC(cons?, nil?))
 
+# A list? is one of:
+# - nil()
+# - cons(AnyC, list?)
 let list? = OrC(cons?, nil?)
 
+# Reverses `before`, appending it onto `acc`. O(before) time and space.
 def rev_app_cons(before: list?, acc: list?) -> list?:
     if cons?(before):
         rev_app_cons(before.cdr, cons(before.car, acc))
     else:
         acc
 
+# Reverses a list. O(lst) time and space.
 def rev_cons(lst: list?) -> list?:
     rev_app_cons(lst, nil())
     
+# Appends two lists, functionally. The resulting list will share structure
+# with `after`. O(before) time and space.
 def app_cons(before: list?, after: list?) -> list?:
     if cons?(before):
         cons(before.car, app_cons(before.cdr, after))
     else:
         after
-        
+
+# Destructively concatenates two lists, returning the concatenated list.
+# O(before) time and O(1) space.
 def concat_cons!(before: list?, after: list?) -> list?:
     if nil?(before): after
     else:
@@ -28,6 +37,7 @@ def concat_cons!(before: list?, after: list?) -> list?:
         current.cdr = after
         before
         
+# Finds the length of a list. O(lst) time and O(1) space.
 def len_cons(lst: list?) -> int?:
     let result = 0
     while cons?(lst):
@@ -35,37 +45,50 @@ def len_cons(lst: list?) -> int?:
         result = result + 1
     result
      
+# Copies a list into a vector starting at index `where`. Assumes there is enough
+# space in the vector. O(lst) time and O(1) space.
 def cons_into_vec(lst: list?, vec: vec?, where: int?) -> VoidC:
-    if cons?(lst):
+    while cons?(lst):
         vec[where] = lst.car
-        cons_into_vec(lst.cdr, vec, where + 1)
-    
+        lst = lst.cdr
+        where = where + 1
+
+# Converts a list to a vector. O(lst) time and space.
 def cons_to_vec(lst: list?) -> vec?:
     let result = [False; len_cons(lst)]
     cons_into_vec(lst, result, 0)
     result
 
+# Creates a list from the contents of a vector starting at the given index.
+# O(|vec| - where) time and space.
 def cons_from_vec_index(vec: vec?, where: int?) -> list?:
     if where < len(vec):
         cons(vec[where], cons_from_vec_index(vec, where + 1))
     else:
         nil()
 
+# Creates a list from the elements of a vector. O(vec) time and space.
 def cons_from_vec(vec: vec?) -> list?:
     cons_from_vec_index(vec, 0)
 
+# Traverses a list from right to left, accumulating a result using the given
+# function. O(lst * f) time and O(lst) space.
 def foldr_cons[Y](f: FunC(AnyC, Y, Y), z: Y, lst: list?) -> Y:
     if cons?(lst):
         f(lst.car, foldr_cons(f, z, lst.cdr))
     else:
         z
         
+# Traverses a list from left to right, accumulating a result using the given
+# function. O(lst * f) time and O(1) space.
 def foldl_cons[Y](f: FunC(Y, AnyC, Y), z: Y, lst: list?) -> Y:
     while cons?(lst):
         z = f(z, lst.car)
         lst = lst.cdr
     z
-    
+
+# Maps a list by applying a function to each element. O(lst) time and O(lst)
+# space (to allocate the new list).
 def map_cons(f: FunC(AnyC, AnyC), lst: list?) -> list?:
     if nil?(lst): nil()
     else:
@@ -78,6 +101,8 @@ def map_cons(f: FunC(AnyC, AnyC), lst: list?) -> list?:
             lst = lst.cdr
         result
 
+# Filters a list by applying a predicate to each element. O(lst) time and O(lst)
+# space (or more precisely, O(result) space).
 def filter_cons(f: FunC(AnyC, AnyC), lst: list?) -> list?:
     while cons?(lst) and !f(lst.car):
        lst = lst.cdr

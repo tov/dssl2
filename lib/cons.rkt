453d3d6ef0b995d4e9bdbfd6ca094c6566f38c9c
Jesse A. Tov <jesse.tov@gmail.com>
2017-08-08 08:09:38 -0500

    Some renaming and tests for cons lists.

--- lib/cons.rkt
+++ lib/cons.rkt
@@ -11,16 +11,16 @@ def rev_app_cons(before: list?, acc: list?) -> list?:
     else:
         acc
 
-def reverse_cons(lst: list?) -> list?:
+def rev_cons(lst: list?) -> list?:
     rev_app_cons(lst, nil())
     
-def append_cons(before: list?, after: list?) -> list?:
+def app_cons(before: list?, after: list?) -> list?:
     if cons?(before):
-        cons(before.car, append_cons(before.cdr, after))
+        cons(before.car, app_cons(before.cdr, after))
     else:
         after
         
-def nconcat_cons!(before: list?, after: list?) -> list?:
+def concat_cons!(before: list?, after: list?) -> list?:
     if nil?(before): after
     else:
         let current = before
@@ -28,31 +28,31 @@ def nconcat_cons!(before: list?, after: list?) -> list?:
         current.cdr = after
         before
         
-def length_cons(lst: list?) -> int?:
+def len_cons(lst: list?) -> int?:
     let result = 0
     while cons?(lst):
         lst = lst.cdr
         result = result + 1
     result
      
-def into_vector(lst: list?, vec: vec?, where: int?) -> VoidC:
+def cons_into_vec(lst: list?, vec: vec?, where: int?) -> VoidC:
     if cons?(lst):
         vec[where] = lst.car
-        into_vector(lst.cdr, vec, where + 1)
+        cons_into_vec(lst.cdr, vec, where + 1)
     
-def to_vector(lst: list?) -> vec?:
-    let result = [False; length_cons(lst)]
-    into_vector(lst, result, 0)
+def cons_to_vec(lst: list?) -> vec?:
+    let result = [False; len_cons(lst)]
+    cons_into_vec(lst, result, 0)
     result
 
-def from_vector_index(vec: vec?, where: int?) -> list?:
+def cons_from_vec_index(vec: vec?, where: int?) -> list?:
     if where < len(vec):
-        cons(vec[where], from_vector_index(vec, where + 1))
+        cons(vec[where], cons_from_vec_index(vec, where + 1))
     else:
         nil()
 
-def from_vector(vec: vec?) -> list?:
-    from_vector_index(vec, 0)
+def cons_from_vec(vec: vec?) -> list?:
+    cons_from_vec_index(vec, 0)
 
 def foldr_cons[Y](f: FunC(AnyC, Y, Y), z: Y, lst: list?) -> Y:
     if cons?(lst):
@@ -67,5 +67,41 @@ def foldl_cons[Y](f: FunC(Y, AnyC, Y), z: Y, lst: list?) -> Y:
     z
     
 def map_cons(f: FunC(AnyC, AnyC), lst: list?) -> list?:
-    foldr_cons(lambda x, y: cons(f(x), y), nil(), lst)
+    if nil?(lst): nil()
+    else:
+        let result = cons(f(lst.car), nil())
+        let current = result
+        lst = lst.cdr
+        while cons?(lst):
+            current.cdr = cons(f(lst.car), nil())
+            current = current.cdr
+            lst = lst.cdr
+        result
+
+def filter_cons(f: FunC(AnyC, AnyC), lst: list?) -> list?:
+    while cons?(lst) and !f(lst.car):
+       lst = lst.cdr
+    if nil?(lst): nil()
+    else:
+        let result = cons(lst.car, nil())
+        let current = result
+        lst = lst.cdr
+        while cons?(lst):
+            if f(lst.car):
+                current.cdr = cons(lst.car, nil())
+                current = current.cdr
+            lst = lst.cdr
+        result
+        
+def cons_tests():
+    let list = cons_from_vec
+     
+    test 'map_cons':
+        def add2(x): x + 2
+        assert_eq map_cons(add2, list([2, 3, 4])), list([4, 5, 6])
+        
+    test 'filter_cons':
+        let lst = list([2, 3, 4, 5, 6])
+        assert_eq filter_cons(even?, lst), list([2, 4, 6])
+        assert_eq filter_cons(odd?, lst), list([3, 5])
     

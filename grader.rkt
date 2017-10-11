94e2dbb01e06ee51902f96f0e0cc29bf891f557a
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-11 16:53:21 -0500

    Grader requires submod file correctly.

--- grader.rkt
+++ grader.rkt
@@ -32,7 +32,7 @@
                    [sandbox-output current-output-port]
                    [sandbox-error-output current-error-port])
       ((make-evaluator 'racket/base
-                       `(require (submod ,subject test-info))
+                       `(require (submod (file ,subject) test-info))
                        #:allow-read (list
                                       "/Applications/Racket"
                                       (current-directory)))

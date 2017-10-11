d837c7bdeb6bbd6ec088f47f265d0ab4064dbdcf
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-10 20:10:08 -0500

    A simple sandboxed (sort of) grading hardness.

--- grader.rkt
+++ grader.rkt
@@ -1,3 +1,13 @@
 #lang racket/base
 
+(provide grade-file)
+
+(require (only-in racket/sandbox
+                  make-evaluator))
+
+(define (grade-file subject)
+  ((make-evaluator 'racket/base
+                   `(require (submod ,subject test-info))
+                   #:allow-read `("/"))
+   '(get-test-info)))
 
--- test/grader.rkt
+++ test/grader.rkt
@@ -1,3 +1,20 @@
 #lang racket/base
 
 (require dssl2/grader)
+(require rackunit
+         syntax/parse/define)
+
+(define (check-grades subject expected-successes expected-total)
+  (define-values (actual-successes actual-total)
+    (grade-file subject))
+  (check-equal? actual-successes expected-successes
+                (format "~a: successes" subject))
+  (check-equal? actual-total expected-total
+                (format "~a: total" subject)))
+
+(check-grades "grader/1_of_1.rkt"
+              1 1)
+(check-grades "grader/2_of_2.rkt"
+              2 2)
+(check-grades "grader/2_of_3.rkt"
+              2 3)

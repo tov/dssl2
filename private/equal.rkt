9666ea314e2e494caf66bd35e124b3a097d3f5cb
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-13 10:35:55 -0500

    discovered that printing is broken.

--- private/equal.rkt
+++ private/equal.rkt
@@ -2,5 +2,48 @@
 
 (provide dssl-equal?)
 
-(define (dssl-equal? a b)
-  (equal? a b))
+(require "struct.rkt")
+
+; A HashPair is (make-hash-pair fixnum? fixnum?)
+(struct hash-pair [left right])
+
+(define (make-key a b)
+  (hash-pair (eq-hash-code a) (eq-hash-code b)))
+
+(define (dssl-equal? a0 b0)
+  (define table #false)
+
+  (define (see! a b)
+    (unless table (set! table (make-hash)))
+    (printf "(see! ~e ~e)\n" a b)
+    (hash-update! table
+                  (make-key a b)
+                  (λ (pairs) (cons (cons a b) pairs))
+                  '()))
+
+  (define (seen? a b)
+    (printf "(seen ~e ~e)\n" a b)
+    (and
+      table
+      (for/or ([pair (hash-ref table (make-key a b) '())])
+        (and (eq? a (car pair))
+             (eq? b (cdr pair))))))
+
+  (define (compare a b)
+    (let-syntax ([both (syntax-rules () [(_ p?) (and (p? a) (p? b))])])
+      (cond
+        ; This case covers equality for booleans, contracts, and
+        ; procedures as well as physically equal pointers.
+        [(eq? a b)              #true]
+        [(both number?)         (= a b)]
+        [(both string?)         (string=? a b)]
+        [(seen? a b)            #true]
+        [(and (both vector?) (= (vector-length a) (vector-length b)))
+         (see! a b)
+         (for/and ([x (in-vector a)]
+                   [y (in-vector b)])
+           (compare x y))]
+        [(both struct-base?)    #false]
+        [else #false])))
+
+  (compare a0 b0))
--- test/dssl2/equal.rkt
+++ test/dssl2/equal.rkt
@@ -2,3 +2,17 @@
 
 assert [2, 3, 4] == [2, 3, 4]
 assert [3, 3, 4] != [2, 3, 4]
+
+assert 2 == 2
+assert 2 != 3
+assert 2 == 2.0
+assert [2] == [2.0]
+
+let cycle1 = [0, 1, 2]
+cycle1[2] = cycle1
+
+let cycle2 = [0, 1, 2]
+cycle2[2] = cycle2
+
+println("~e", cycle1)
+# assert cycle1 == cycle2

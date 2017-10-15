b8d153310aab86d73acfce61cdd6879f3dee1d9a
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-15 10:41:49 -0500

    Tests for parametric contracts.

---
+++ test/dssl2/parametric.rkt
@@ -0,0 +1,23 @@
+#lang dssl2
+
+def id[A](x: A) -> A: x
+
+assert_eq id(5), 5
+
+def choose[A, B](which: bool?, x: A, y: B) -> OrC(A, B):
+    x if which else y
+
+assert_eq choose(True, 4, 'hello'), 4
+assert_eq choose(False, 4, 'hello'), 'hello'
+
+# assert_eq format('~e', choose), 'choose'
+
+def tricky[Y](good?: bool?, x: Y) -> Y:
+    x if good? else 4
+
+assert_eq tricky(True, 5), 5
+assert_error tricky(False, 5), 'promised: Y'
+
+tricky = λ x, y: x
+
+assert_error tricky(True, 5), 'promised: Y'

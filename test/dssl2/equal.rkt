381295a6431e6ae10d0a1d23a531e2db5a49c2d5
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-15 08:36:12 -0500

    Rearranged tests

--- test/dssl2/equal.rkt
+++ test/dssl2/equal.rkt
@@ -8,6 +8,11 @@ assert 2 != 3
 assert 2 == 2.0
 assert [2] == [2.0]
 
+defstruct posn(x, y)
+
+assert posn(3, 4) == posn(3, 4)
+assert posn(3, 4) != posn(3, 5)
+
 let cycle1 = [0, 1, 2]
 cycle1[2] = cycle1
 
@@ -15,8 +20,3 @@ let cycle2 = [0, 1, 2]
 cycle2[2] = cycle2
 
 assert cycle1 == cycle2
-
-defstruct posn(x, y)
-
-assert posn(3, 4) == posn(3, 4)
-assert posn(3, 4) != posn(3, 5)

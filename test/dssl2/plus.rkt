82ce0f1ac88531166bcf328ad4b7d9e763862f14
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-15 08:40:27 -0500

    cleaned up + tests

--- test/dssl2/plus.rkt
+++ test/dssl2/plus.rkt
@@ -5,6 +5,9 @@ assert 2 + "four" == "2four"
 assert "two" + 4 == "two4"
 assert "two" + "four" == "twofour"
 
-assert "" + True == 'True' # bad
+assert "" + True == 'True'
 
 assert inf + 'hi' == 'infhi'
+assert 'hi' + nan == 'hinan'
+assert inf + inf == inf
+

5591b4098e5616a5e48ac7ab502e81fa478b027a
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-17 15:33:16 -0500

    Doc links work!

--- private/parser.rkt
+++ private/parser.rkt
@@ -17,21 +17,6 @@
    (new-dssl2-lexer src port interactive?)))
 
 
-(define (read-symbol* sym pos)
-  (let ([port (open-input-string (format "~s" sym))])
-    (port-count-lines! port)
-    (set-port-next-location! port
-                             (position-line pos)
-                             (position-col pos)
-                             (position-offset pos))
-    (read-syntax #f port)))
-
-(define-syntax (read-symbol stx)
-  (syntax-case stx ()
-    [(_ sym)
-     (with-syntax [(start (datum->syntax stx '$1-start-pos))]
-       #'(read-symbol* sym start))]))
-
 (define (dssl2-parser src)
   (define (parser-error tok-ok? tok-name tok-value start-pos end-pos)
     (raise-read-error (format "Syntax error: unexpected token ‘~a’"
@@ -60,6 +45,29 @@
        (with-syntax [(start (datum->syntax stx '$1-start-pos))
                      (end   (datum->syntax stx '$n-end-pos))]
          #'(locate start end sexp))]))
+
+  (define-syntax (sym stx)
+    (syntax-case stx ()
+      [(_ sexp)
+       (with-syntax [(start (datum->syntax stx '$1-start-pos))
+                     (end   (datum->syntax stx '$n-end-pos))]
+         (syntax-property #'(locate start end sexp)
+                          'original-for-check-syntax #t))]))
+
+  (define (symbol/pos sym pos)
+    (let ([port (open-input-string (format "~s" sym))])
+      (port-count-lines! port)
+      (set-port-next-location! port
+                               (position-line pos)
+                               (position-col pos)
+                               (position-offset pos))
+      (read-syntax src port)))
+
+  (define-syntax (symbol stx)
+    (syntax-case stx ()
+      [(_ sym)
+       (with-syntax [(start (datum->syntax stx '$1-start-pos))]
+         #'(symbol/pos sym start))]))
 
   (parser
     (tokens dssl2-empty-tokens dssl2-tokens)
@@ -104,27 +112,27 @@
 
       (<compound-statement>
         [(IF <expr0> COLON <suite> <elifs> <maybe-else>)
-         (loc `(,(read-symbol 'cond)
+         (loc `(,(symbol 'if)
                  [,$2 ,@$4]
                  ,@$5
                  ,$6))]
         [(WHILE <expr0> COLON <suite>)
-         (loc `(,(read-symbol 'while) ,$2 ,@$4))]
+         (loc `(,(symbol 'while) ,$2 ,@$4))]
         [(FOR <ident> IN <expr> COLON <suite>)
-         (loc `(,(read-symbol 'for) [,$2 ,$4] ,@$6))]
+         (loc `(,(symbol 'for) [,$2 ,$4] ,@$6))]
         [(FOR <ident> COMMA <ident> IN <expr> COLON <suite>)
-         (loc `(,(read-symbol 'for) [(,$2 ,$4) ,$6] ,@$8))]
+         (loc `(,(symbol 'for) [(,$2 ,$4) ,$6] ,@$8))]
         [(DEF <ident> <foralls> LPAREN <contract-formals> RPAREN <result>
               COLON <suite>)
-         (loc `(,(read-symbol 'def) (,$2 ,@$3 ,@$5) ,@$7 ,@$9))]
+         (loc `(,(symbol 'def) (,$2 ,@$3 ,@$5) ,@$7 ,@$9))]
         [(TEST <expr> COLON <suite>)
-         (loc `(,(read-symbol 'test) ,$2 ,@$4))]
+         (loc `(,(symbol 'test) ,$2 ,@$4))]
         [(TEST COLON <suite>)
-         (loc `(,(read-symbol 'test) "<anonymous-test>" ,@$3))]
+         (loc `(,(symbol 'test) "<anonymous-test>" ,@$3))]
         [(TIME <expr> COLON <suite>)
-         (loc `(,(read-symbol 'time) ,$2 ,@$4))]
+         (loc `(,(symbol 'time) ,$2 ,@$4))]
         [(TIME COLON <suite>)
-         (loc `(,(read-symbol 'time) "<anonymous-time>" ,@$3))])
+         (loc `(,(symbol 'time) "<anonymous-time>" ,@$3))])
 
       (<elifs>
         [()
@@ -140,7 +148,7 @@
         [()
          `[else (pass)]]
         [(ELSE COLON <suite>)
-         (loc `[,(read-symbol 'else) ,@$3])])
+         (loc `[,(symbol 'else) ,@$3])])
 
       (<result>
         [(ARROW <expr>)
@@ -172,33 +180,33 @@
         [(<expr>)
          $1]
         [(LET <contract-formal>)
-         (loc `(,(read-symbol 'let) ,$2))]
+         (loc `(,(symbol 'let) ,$2))]
         [(LET <contract-formal> EQUALS <expr>)
-         (loc `(,(read-symbol 'let) ,$2 ,$4))]
+         (loc `(,(symbol 'let) ,$2 ,$4))]
         [(DEFSTRUCT <ident> LPAREN <contract-formals> RPAREN)
-         (loc `(,(read-symbol 'defstruct) ,$2 ,$4))]
+         (loc `(,(symbol 'defstruct) ,$2 ,$4))]
         [(BREAK)
-         (loc `(,(read-symbol 'break)))]
+         (loc `(,(symbol 'break)))]
         [(CONTINUE)
-         (loc `(,(read-symbol 'continue)))]
+         (loc `(,(symbol 'continue)))]
         [(IMPORT <ident>)
-         (loc `(,(read-symbol 'import) ,$2))]
+         (loc `(,(symbol 'import) ,$2))]
         [(IMPORT STRING-LITERAL)
-         (loc `(,(read-symbol 'import) ,$2))]
+         (loc `(,(symbol 'import) ,$2))]
         [(RETURN <expr>)
-         (loc `(,(read-symbol 'return) ,$2))]
+         (loc `(,(symbol 'return) ,$2))]
         [(RETURN)
-         (loc `(,(read-symbol 'return)))]
+         (loc `(,(symbol 'return)))]
         [(<lvalue> EQUALS <expr>)
-         (loc `(,(read-symbol 'setf!) ,$1 ,$3))]
+         (loc `(,(symbol/pos '= $2-start-pos) ,$1 ,$3))]
         [(ASSERT <expr>)
-         (loc `(,(read-symbol 'assert) ,$2))]
+         (loc `(,(symbol 'assert) ,$2))]
         [(ASSERT-EQ <expr> COMMA <expr>)
-         (loc `(,(read-symbol 'assert_eq) ,$2 ,$4))]
+         (loc `(,(symbol 'assert_eq) ,$2 ,$4))]
         [(ASSERT-ERROR <expr>)
-         (loc `(,(read-symbol 'assert_error) ,$2))]
+         (loc `(,(symbol 'assert_error) ,$2))]
         [(ASSERT-ERROR <expr> COMMA STRING-LITERAL)
-         (loc `(,(read-symbol 'assert_error) ,$2 ,$4))]
+         (loc `(,(symbol 'assert_error) ,$2 ,$4))]
         [(PASS)
          (loc `(pass))])
 
@@ -232,7 +240,7 @@
 
       (<ident>
         [(IDENT)
-         (read-symbol $1)])
+         (symbol $1)])
 
       (<lvalue>
         [(<ident>)
@@ -252,21 +260,21 @@
         [(<atom> LPAREN <actuals> RPAREN)
          (loc `(,$1 ,@$3))]
         [(LBRACK <actuals> RBRACK)
-         (loc `(,(read-symbol 'vec) ,@$2))]
+         (loc `(vec ,@$2))]
         [(LBRACK <expr> SEMICOLON <expr> RBRACK)
-         (loc `(,(read-symbol 'make-vec) ,$4 ,$2))]
+         (loc `(make-vec ,$4 ,$2))]
         [(LBRACK <expr> FOR <ident> IN <expr0> RBRACK)
-         (loc `(,(read-symbol 'for/vec) [,$4 ,$6] ,$2))]
+         (loc `(for/vec [,$4 ,$6] ,$2))]
         [(LBRACK <expr> FOR <ident> COMMA <ident> IN <expr0> RBRACK)
-         (loc `(,(read-symbol 'for/vec) [(,$4 ,$6) ,$8] ,$2))]
+         (loc `(for/vec [(,$4 ,$6) ,$8] ,$2))]
         [(LBRACK <expr> FOR <ident> IN <expr0> IF <expr> RBRACK)
-         (loc `(,(read-symbol 'for/vec) [,$4 ,$6] #:when ,$8 ,$2))]
+         (loc `(for/vec [,$4 ,$6] #:when ,$8 ,$2))]
         [(LBRACK <expr> FOR <ident> COMMA <ident> IN <expr0> IF <expr> RBRACK)
-         (loc `(,(read-symbol 'for/vec) [(,$4 ,$6) ,$8] #:when ,$10 ,$2))]
+         (loc `(for/vec [(,$4 ,$6) ,$8] #:when ,$10 ,$2))]
         [(<ident> LBRACE <fields> RBRACE)
          (loc `(,(format-id #f "m:~a" $1 #:source $1) ,@$3))]
         [(OBJECT <ident> LBRACE <fields> RBRACE)
-         (loc `(,(read-symbol 'object) ,$2 ,@$4))]
+         (loc `(,(symbol 'object) ,$2 ,@$4))]
         [(LPAREN <expr> RPAREN)
          (loc $2)])
 
@@ -294,82 +302,82 @@
 
       (<expr>
         [(LAMBDA <formals> COLON <single-line-statement>)
-         (loc `(,(read-symbol 'lambda) ,$2 ,@$4))]
+         (loc `(,(symbol 'lambda) ,$2 ,@$4))]
         [(<expr0> IF <expr0> ELSE <expr>)
-         (loc `(,(read-symbol 'if) ,$3 ,$1 ,$5))]
+         (loc `(if-e ,$3 ,$1 ,$5))]
         [(<expr0>)
          $1])
 
       (<expr0>
         [(<expr0> OP0 <expr1>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr1>)
          $1])
 
       (<expr1>
         [(<expr1> OP1 <expr2>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr2>)
          $1])
 
       (<expr2>
         [(<expr3> OP2 <expr3>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr3>)
          $1])
 
       (<expr3>
         [(<expr3> OP3 <expr4>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr4>)
          $1])
 
       (<expr4>
         [(<expr4> OP4 <expr5>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr5>)
          $1])
 
       (<expr5>
         [(<expr5> OP5 <expr6>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr6>)
          $1])
 
       (<expr6>
         [(<expr6> OP6 <expr7>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr7>)
          $1])
 
       (<expr7>
         [(<expr7> PLUS <expr8>)
-         (loc `(,(read-symbol* '+ $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos '+ $2-start-pos) ,$1 ,$3))]
         [(<expr7> MINUS <expr8>)
-         (loc `(,(read-symbol* '- $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos '- $2-start-pos) ,$1 ,$3))]
         [(<expr7> OP7 <expr8>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr8>)
          $1])
 
       (<expr8>
         [(<expr8> OP8 <expr9>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<expr9>)
          $1])
 
       (<expr9>
         [(OP9 <expr9>)
-         (loc `(,(read-symbol $1) ,$2))]
+         (loc `(,(symbol $1) ,$2))]
         [(PLUS <expr9>)
-         (loc `(,(read-symbol '+) ,$2))]
+         (loc `(,(symbol '+) ,$2))]
         [(MINUS <expr9>)
-         (loc `(,(read-symbol '-) ,$2))]
+         (loc `(,(symbol '-) ,$2))]
         [(<expr10>)
          $1])
 
       (<expr10>
         [(<atom> OP10 <expr10>)
-         (loc `(,(read-symbol* $2 $2-start-pos) ,$1 ,$3))]
+         (loc `(,(symbol/pos $2 $2-start-pos) ,$1 ,$3))]
         [(<atom>)
          $1]))))
--- test/parser.rkt
+++ test/parser.rkt
@@ -59,20 +59,20 @@
   ; simple statements
 
   (check-parse? "a = b"
-                (setf! a b))
+                (= a b))
   (check-parse? "a = b; c = d.e"
-                (setf! a b)
-                (setf! c (struct-ref d e)))
+                (= a b)
+                (= c (struct-ref d e)))
   (check-parse? "a = b\nc = d\n"
-                (setf! a b)
-                (setf! c d))
+                (= a b)
+                (= c d))
   (check-parse? "let x"
                 (let x))
   (check-parse? "defstruct posn(x, y)"
                 (defstruct posn (x y)))
   (check-parse? "a.b.c = e[f]"
-                (setf! (struct-ref (struct-ref a b) c)
-                       (vec-ref e f)))
+                (= (struct-ref (struct-ref a b) c)
+                   (vec-ref e f)))
   (check-parse? "assert False"
                 (assert #f))
   (check-parse? "assert_eq a + 1, 6"
@@ -81,32 +81,32 @@
   ; compound statements
 
   (check-parse? "if a: c = d"
-                (cond [a (setf! c d)] [else (pass)]))
+                (if [a (= c d)] [else (pass)]))
   (check-parse? "if a: c = d\nelse: e = f"
-                (cond [a (setf! c d)] [else (setf! e f)]))
+                (if [a (= c d)] [else (= e f)]))
   (check-parse? "if a: c = d\nelif b: e = 3\nelse: f = 4"
-                (cond [a (setf! c d)]
-                      [b (setf! e 3)]
-                      [else (setf! f 4)]))
+                (if [a (= c d)]
+                    [b (= e 3)]
+                    [else (= f 4)]))
   (check-parse? "if a:\n  c = d"
-                (cond [a (setf! c d)]
-                      [else (pass)]))
+                (if [a (= c d)]
+                    [else (pass)]))
   (check-parse? "if a:\n  c = d\n  e[0] = 9"
-                (cond [a (setf! c d)
-                         (setf! (vec-ref e 0) 9)]
-                      [else (pass)]))
+                (if [a (= c d)
+                       (= (vec-ref e 0) 9)]
+                    [else (pass)]))
   (check-parse? "if a:\n  if b:\n    5"
-                (cond [a (cond [b 5]
-                               [else (pass)])]
-                      [else (pass)]))
+                (if [a (if [b 5]
+                             [else (pass)])]
+                    [else (pass)]))
   (check-parse? "while True:\n  a = 6\n  b = 7"
-                (while #t (setf! a 6) (setf! b 7)))
+                (while #t (= a 6) (= b 7)))
   (check-parse? (string-append "def fact(n):\n"
                                "  if n <= 1: return 1\n"
                                "  else: return n * fact(n - 1)")
                 (def (fact n)
-                     (cond [(<= n 1) (return 1)]
-                           [else     (return (* n (fact (- n 1))))])))
+                     (if [(<= n 1) (return 1)]
+                         [else     (return (* n (fact (- n 1))))])))
   (check-parse? (string-append "for j in v:\n"
                                "  println(j)")
                 (for [j v] (println j)))

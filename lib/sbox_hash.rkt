ee9683d3bb0b90c8c462414fbe2535fc8452331d
Jesse A. Tov <jesse.tov@gmail.com>
2018-05-01 09:08:40 -0500

    Made lib/sbox_hash handle characters > 255.

--- lib/sbox_hash.rkt
+++ lib/sbox_hash.rkt
@@ -3,11 +3,11 @@
 def HashFunctionC(X): FunC(X, int?)
 
 def make_sbox_hash() -> HashFunctionC(str?):
-    let sbox = [ random_bits(64) for i in 256 ]
+    let sbox = [ random_bits(64) for _ in 256 ]
     def hash(input_string):
         let result = 0
         for c in input_string:
-            let svalue = sbox[ord(c)]
+            let svalue = sbox[ord(c) % 256]
             result = result ^ svalue
             result = (3 * result) % (2 ** 64)
         return result

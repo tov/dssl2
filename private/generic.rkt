072bd6f650168db2a62aecd8c89aa3e07803a162
Jesse A. Tov <jesse.tov@gmail.com>
2018-07-12 19:25:37 -0500

    Cleaned up some contract error messages. Added comments to private/generic.rkt.

--- private/generic.rkt
+++ private/generic.rkt
@@ -39,12 +39,12 @@
                      (only-in racket/syntax generate-temporary)))
 
 ; This is for generic things that can be instantiated
-; with square brackets.
+; with square brackets. They also have names for printing.
 (struct generic-base (name instantiate))
 
 ; Generic procs are functions with optional square-bracket
 ; arguments. That is, they can be instantiated or directly
-; applied.
+; applied. The tag is 'class or 'proc, for printing.
 (struct generic-proc generic-base (apply tag)
   #:property prop:procedure
   (struct-field-index apply))
@@ -58,6 +58,9 @@
     #:first-order (λ (v) (generic-contract-first-order v))
     #:late-neg-projection (λ (v) (generic-contract-late-neg-projection v))))
 
+; format-generic : symbol? [List-of contract?] -> symbol?
+; Formats the application of a generic procedure to its optional (square
+; bracket) arguments.
 (define (format-generic f xs)
   (define port (open-output-string))
   (fprintf port "~a[~a" f (contract-name (car xs)))
@@ -66,6 +69,17 @@
   (fprintf port "]")
   (string->symbol (get-output-string port)))
 
+; Creates a class, which may or may not have generic parameters.
+; If there are generic parameters, they must be given defaults.
+; Here we create a generic function that can be called like
+; `Pair[T, U](fst, snd)` or `Pair(fst, snd)`. In the latter
+; case, `T` and `U` default to `AnyC`:
+;
+;     (square-bracket-class-lambda
+;       Pair
+;       ([T AnyC] [U AnyC])
+;       (fst snd)
+;       ...body...)
 (define-syntax (square-bracket-class-lambda stx)
   (syntax-parse stx
     [(_ name:id
@@ -88,6 +102,17 @@
                        (instantiate default ...)
                        'class))]))
 
+; Creates a procedure that can be applied with square brackets or
+; without.
+;
+;     (square-bracket-proc make_pair
+;       #:generic (T U) ...body_1...
+;       #:default ...body_2...
+;
+; In the above example, `make_pair[V, W](x)` means `...body_1...[V/T,
+; W/U](x)`, whereas `make_pair(x)` means `...body_2...(x)`. That is,
+; both bodies must evaluate to procedures, and `T` and `U` may be used
+; in `...body_1...` only.
 (define-syntax (square-bracket-proc stx)
   (syntax-parse stx
     [(_ name:id
@@ -106,9 +131,28 @@
         #:default default:expr)
      #'default]))
 
+; compose-neg-party : String Syntax -> String
+; This is a helper for define-square-bracket-proc that is used
+; for creating negative parties once they become known. It must
+; be given a string describing what kind of negative party it is
+; and a string whose source location describes where.
 (define-for-syntax (compose-neg-party who where)
   (format "~a at ~a" who (srcloc->string (get-srcloc/fn where))))
 
+; For defining a user square bracket proc, having optional contract
+; params that default to `AnyC`. The syntax is
+;
+;     (define-square-bracket-proc
+;       ((make_pair T U) [fst T] [snd U])
+;       Pair?
+;       ...body...)
+;
+; That is, the above defines `make_pair` as a a curried function that
+; first takes two generic contract parameters `T` and `U`, and then
+; takes two ordinary parameters, `fst` (which is protected by contract
+; `T`) and `snd` (which is protected by conrract `U`). In DSSL2, `T` and
+; `U` must be supplied as square bracket parameters, and default to
+; `AnyC` if `make_pair` is applied with parentheses.
 (define-syntax (define-square-bracket-proc stx)
   (syntax-parse stx
     [(_ ((name:id opt-formal:id ...) [formal:id formal-ctc:expr] ...)
@@ -120,7 +164,7 @@
                      (procedure-rename
                        (λ (formal ...) body)
                        'name)
-                     (format "function ~a at ~a" 'name
+                     (format "def ~a at ~a" 'name
                              (srcloc->string (get-srcloc name)))
                      neg-party
                      'name (get-srcloc name)))
@@ -153,6 +197,15 @@
                       (syntax/loc stx
                         (real-definition neg-party)))])))))]))
 
+; Creates a contract for a procedure that accepts optional, square
+; bracket arguments.
+;
+; Example:
+;
+;     (square-bracket-proc-contract make_pair [T U] T U Pair?)
+;
+; Here, the optional parameters `T` and `U` give contracts for the
+; required parameters, and default to `AnyC`.
 (define-syntax (square-bracket-proc-contract stx)
   (syntax-parse stx
     [(_ name:id [] formal:expr ... result:expr)
@@ -172,20 +225,20 @@
              [(first-order? value)
               (generic-proc
                 (generic-base-name value)
-                (contract (->i ([opt-formal any/c] ...)
-                               (_ (opt-formal ...)
-                                  (-> formal ... result)))
-                          (generic-base-instantiate value)
-                          (blame-positive blame)
-                          missing-party
-                          'name #f)
-                (contract
-                  (let ([opt-formal any/c] ...)
-                    (-> formal ... result))
-                  (generic-proc-apply value)
-                  (blame-positive blame)
-                  missing-party
-                  'name #f)
+                (λ (opt-formal ...)
+                   (contract
+                     (-> formal ... result)
+                     ((generic-base-instantiate value) opt-formal ...)
+                     (blame-positive blame)
+                     missing-party
+                     'name #f))
+                (let ([opt-formal any/c] ...)
+                  (contract
+                    (-> formal ... result)
+                    (generic-proc-apply value)
+                    (blame-positive blame)
+                    missing-party
+                    'name #f))
                 (generic-proc-tag value))]
              [else
                (raise-blame-error
@@ -194,6 +247,10 @@
          (make-contract #:first-order first-order?
                         #:late-neg-projection late-neg-proj))]))
 
+; Creates a contract that accepts optional, square bracket parameters.
+; The contract is specified by giving a sequence of optional parameters
+; with defaults, and then the first-order check and late negative
+; projection.
 (define-syntax (square-bracket-contract stx)
   (syntax-parse stx
     [(_ name:id ()
@@ -217,6 +274,10 @@
            (make-first-order default ...)
            (make-projection default ...)))]))
 
+; Creates a contract that takes optional, square bracket parameters.
+; Unlike square-bracket-contract, this allows specifying different
+; contracts for the case where optional parameters are given, and
+; for the case where they aren't.
 (define-syntax (special-square-bracket-contract stx)
   (syntax-parse stx
     [(_ name:id
@@ -232,7 +293,7 @@
            (contract-first-order ctc)
            (contract-late-neg-projection ctc)))]
     [(_ name:id
-        #:generic (opt-formal:id ...+ . rest:id) generic:expr
+        #:generic (opt-formal:id ... . rest:id) generic:expr
         #:default default:expr)
      #'(let ([ctc default])
          (generic-contract
@@ -241,18 +302,6 @@
               (rename-contract
                 generic
                 (format-generic 'name (list* opt-formal ... rest))))
-           (contract-first-order ctc)
-           (contract-late-neg-projection ctc)))]
-    [(_ name:id
-        #:generic opt-formals:id generic:expr
-        #:default default:expr)
-     #'(let ([ctc default])
-         (generic-contract
-           'name
-           (λ opt-formals
-              (rename-contract
-                generic
-                (format-generic 'name opt-formals)))
            (contract-first-order ctc)
            (contract-late-neg-projection ctc)))]))
 

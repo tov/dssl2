75c03a7f3c69bd65e83f28712763cd70cc649264
Jesse A. Tov <jesse.tov@gmail.com>
2018-09-04 08:57:41 -0500

    Added a field.

--- test/dssl2/interface-import.rkt
+++ test/dssl2/interface-import.rkt
@@ -6,7 +6,8 @@ interface J (I):
     def m3(self, _, _)
     
 class MyJ (J):
-    def __init__(self): pass
+    let _inner
+    def __init__(self): self._inner = False
     def m1(self, x): pass
     def m2(self, x): pass
-    def m3(self, x, y): pass
+    def m3(self, x, y): self._inner

4b43f8d72cb10bdf339b46e4c73151f081815597
Jesse A. Tov <jesse.tov@gmail.com>
2018-09-05 08:53:16 -0500

    Cleaned up how dssl-provide works.

--- private/provide.rkt
+++ private/provide.rkt
@@ -5,19 +5,21 @@
                      syntax/parse
                      "names.rkt"))
 
+(define-syntax (provide1 stx)
+  (syntax-parse stx #:literals (for-syntax)
+    [(_ name:id) #:when (public-method-name? #'name)
+      #'(provide name)]
+    [(_ (for-syntax name:id)) #:when (public-method-name? #'name)
+      #'(provide (for-syntax name))]
+    [_
+      #'(begin)]))
+
 (define-syntax (dssl-provide stx)
-  (define (each-spec spec)
-    (syntax-parse spec #:literals (for-syntax)
-      [name:id #:when (public-method-name? #'name)
-        #'(provide name)]
-      [(for-syntax name:id) #:when (public-method-name? #'name)
-        #'(provide (for-syntax name))]
-      [_
-        #'(begin)]))
   (if (eq? 'module (syntax-local-context))
     (syntax-parse stx
       [(_ spec ...)
-       #`(begin
-           #,@(map each-spec (syntax->list #'(spec ...))))])
+       #'(begin
+           (provide1 spec)
+           ...)])
     #'(begin)))
 

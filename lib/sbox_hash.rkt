43740948ec07d6f13f73d3ad0e4ed646bd8c4b5d
Jesse A. Tov <jesse.tov@gmail.com>
2019-06-01 18:31:39 -0500

    changed make_sbox_hash function to SboxHash class.

--- lib/sbox_hash.rkt
+++ lib/sbox_hash.rkt
@@ -1,14 +1,48 @@
 #lang dssl2
 
-def HashFunctionC(X): FunC[X, int?]
+# Each instance is a randomly generated hash function. The constructor
+# takes the bit length for the resulting hash codes. The only method,
+# `apply`, hashes a string to an integer of the requested bit length.
+class SboxHash:
+    let hash_max: nat?
+    let start:    nat?
+    let sbox:     vec?
 
-def make_sbox_hash() -> HashFunctionC(str?):
-    let sbox = [ random_bits(64) for _ in 256 ]
-    def hash(input_string):
-        let result = 0
+    def __init__(self, hash_bits: nat?):
+        self.hash_max = 2 ** hash_bits - 1
+        self.start    = random_bits(hash_bits)
+        self.sbox     = [ random_bits(hash_bits) for _ in 256 ]
+
+    # Maps a character to its randomized hash code.
+    def _substitute(self, c: char?) -> nat?:
+        self.sbox[int(c) % self.sbox.len()]
+     
+    # Combines a character with the hash code, by `xor`ing its
+    # substitution value into the hash code.
+    def _combine(self, hash_code: nat?, c: char?) -> nat?:
+        hash_code ^ self._substitute(c)
+    
+    # Mixes a hash code to ensure that each combined character affects
+    # different bits.    
+    def _mix(self, hash_code: nat?) -> nat?:
+        3 * hash_code 
+
+    # Applies the hash function.
+    def apply(self, input_string: str?) -> nat?:
+        let hash_code = self.start
         for c in input_string:
-            let svalue = sbox[int(c) % 256]
-            result = result ^ svalue
-            result = (3 * result) % (2 ** 64)
-        return result
-    hash
+            hash_code = self._mix(self._combine(hash_code, c)) & self.hash_max
+        return hash_code
+      
+    # Two `SboxHash`es are only equal if they have the same object
+    # identity.
+    def __eq__(self, other) -> bool?:
+        self is other
+        
+    # Prints an `SboxHash` without showing the sbox (because it's big).
+    def __print__(self, print):
+        print("#<object:SboxHash hash_max=%p start=%p sbox=...>",
+              self.hash_max, self.start)
+
+# Creates a 64-bit hash function
+def SboxHash64(): SboxHash(64)

1c236be69786da5ed7f179be4d27403cd9d2116b
Vincent St-Amour <stamourv@racket-lang.org>
2019-09-04 15:09:51 -0500

    Split docs into multiple pages.

--- info.rkt
+++ info.rkt
@@ -6,7 +6,7 @@
 (define version "2.14.0")
 (define deps '("base" "rackunit-lib" "parser-tools-lib" "sandbox-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/dssl2.scrbl" ())))
+(define scribblings '(("scribblings/dssl2.scrbl" (multi-page))))
 (define test-omit-paths '("test/dssl2" "test/grader"))
 (define blurb
     '("A language for data structures students, version 2"))

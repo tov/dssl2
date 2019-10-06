98a297756127c6628ba86f0b5d92a2f0030891aa
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-06 15:30:07 -0500

    more helpers for dealing with falsiness of None.

--- private/singletons.rkt
+++ private/singletons.rkt
@@ -4,7 +4,12 @@
          dssl-False
          dssl-None
          falsy?
-         truthy?)
+         truthy?
+         falsy->false
+         truthy-cond)
+
+(require (for-syntax racket/base)
+         syntax/parse/define)
 
 (define dssl-True #t)
 (define dssl-False #f)
@@ -16,4 +21,18 @@
 (define (truthy? v)
   (not (falsy? v)))
 
+(define (falsy->false v)
+  (and (truthy? v) v))
+
+(begin-for-syntax
+  (define-syntax-class truthy-clause
+    #:literals (else)
+    (pattern [els:else body:expr ...+]
+             #:attr translation #'[els body ...])
+    (pattern [test:expr bodyish:expr ...]
+             #:attr translation #'[(falsy->false test) bodyish ...])))
+
+(define-syntax-parser truthy-cond
+  [(_ clause:truthy-clause ...)
+   #'(cond clause.translation ...)])
 

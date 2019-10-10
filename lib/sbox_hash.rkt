0b64198977584f5355308986ba0056f1d65009da
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-09 21:25:06 -0500

    sbox now depends on uint64.

--- lib/sbox_hash.rkt
+++ lib/sbox_hash.rkt
@@ -1,40 +1,46 @@
 #lang dssl2
 
-# Each instance is a randomly generated hash function. The constructor
-# takes the bit length for the resulting hash codes. The only method,
-# `apply`, hashes the string representation of its input to an integer
-# of the requested bit length.
-class SboxHash:
-    let hash_max: nat?
-    let start:    nat?
-    let sbox:     vec?
+import uint64
 
-    def __init__(self, hash_bits: nat?):
-        self.hash_max = 2 ** hash_bits - 1
-        self.start    = random_bits(hash_bits)
-        self.sbox     = [ random_bits(hash_bits) for _ in 256 ]
+# Interface for an object providing a hash function.
+interface HASHER:
+    # Hashes `input`.
+    def hash(self, input: AnyC) -> nat?
+
+def _random_uint64():
+    uint64(random_bits(64))
+
+# Each instance is a randomly generated 64-bit hash function. The 
+# only method, `hash`, hashes the string representation of its
+# input to an unsigned 64-bit integer.
+class SboxHash64 (HASHER):
+    let _start:   uint64?
+    let _sbox:    vec?
+
+    def __init__(self):
+        self._start   = _random_uint64()
+        self._sbox    = [ _random_uint64() for _ in 256 ]
 
     # Maps a character to its randomized hash code.
-    def _substitute(self, c: char?) -> nat?:
-        self.sbox[int(c) % self.sbox.len()]
+    def _substitute(self, c: char?) -> uint64?:
+        self._sbox[int(c) % self._sbox.len()]
      
     # Combines a character with the hash code, by `xor`ing its
     # substitution value into the hash code.
-    def _combine(self, hash_code: nat?, c: char?) -> nat?:
+    def _combine(self, hash_code: uint64?, c: char?) -> uint64?:
         hash_code ^ self._substitute(c)
     
     # Mixes a hash code to ensure that each combined character affects
     # different bits.    
-    def _mix(self, hash_code: nat?) -> nat?:
+    def _mix(self, hash_code: uint64?) -> uint64?:
         3 * hash_code 
 
     # Applies the hash function.
-    def apply(self, input: AnyC) -> nat?:
-        let input_string = '%d'.format(input)
-        let hash_code = self.start
-        for c in input_string:
-            hash_code = self._mix(self._combine(hash_code, c)) & self.hash_max
-        return hash_code
+    def hash(self, input: AnyC) -> nat?:
+        let hash_code = self._start
+        for c in str(input):
+            hash_code = self._mix(self._combine(hash_code, c))
+        return hash_code.as_int()
       
     # Two `SboxHash`es are only equal if they have the same object
     # identity.
@@ -43,11 +49,8 @@ class SboxHash:
         
     # Prints an `SboxHash` without showing the sbox (because it's big).
     def __print__(self, print):
-        print("#<object:SboxHash hash_max=%p start=%p sbox=...>",
-              self.hash_max, self.start)
+        print("#<object:SboxHash _start=%p _sbox=...>", self._start)
 
-# Creates a 64-bit hash function
-def SboxHash64(): SboxHash(64)
+def SboxHash(_ignored): SboxHash64()
 
-def HashFunctionC(X): FunC[X, int?]
-def make_sbox_hash(): return SboxHash64().apply
+def make_sbox_hash(): SboxHash().hash

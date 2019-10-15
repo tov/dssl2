21f3f01f4f4487bddb408ea879c17cfa6fe84b2c
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-14 21:39:31 -0500

    release v2.19.0

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define categories '(devtools))
 (define can-be-loaded-with 'all)
 (define required-core-version "6.10")
-(define version "2.18.0")
+(define version "2.19.0")
 (define deps '("base" "rackunit-lib" "parser-tools-lib" "sandbox-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/dssl2.scrbl" (multi-page))))

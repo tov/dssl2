2441c72069c62870421d614b214c8c066695bfb2
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-22 01:55:28 -0500

    Declared dependency on gui-lib.

--- info.rkt
+++ info.rkt
@@ -3,12 +3,16 @@
 (define categories '(devtools))
 (define can-be-loaded-with 'all)
 (define required-core-version "6.10")
-(define version "2.19.2")
-(define deps '("base" "rackunit-lib" "parser-tools-lib" "sandbox-lib"))
+(define version "2.19.3")
+(define deps '("base"
+               "gui-lib"
+               "rackunit-lib"
+               "parser-tools-lib"
+               "sandbox-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/dssl2.scrbl" (multi-page))))
 (define test-omit-paths '("test/dssl2" "test/grader"))
 (define blurb
-    '("A language for data structures students, version 2"))
+  '("A language for data structures students, version 2"))
 (define release-notes
-    '((p "First release")))
+  '((p "No longer the first release")))

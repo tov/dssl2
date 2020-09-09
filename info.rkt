5ec3a4450a0e97e800ff63146c20b07e953d1e17
Jesse A. Tov <jesse.tov@gmail.com>
2020-09-08 20:06:04 -0500

    Minor version bump.

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define categories '(devtools))
 (define can-be-loaded-with 'all)
 (define required-core-version "6.10")
-(define version "2.20.5")
+(define version "2.21.0")
 (define deps '("base"
                "gui-lib"
                "rackunit-lib"

8fd203274dd5d4a8b8f9972eed452f0b76aae998
Vincent St-Amour <stamourv@racket-lang.org>
2022-12-20 10:36:06 -0600

    Better error message for undefined variables.
    Thank you to Shu-Hung You for the macrology.

--- language.rkt
+++ language.rkt
@@ -1,12 +1,20 @@
 #lang racket/base
 
 (provide #%datum
-         #%top
          #%require
+         (rename-out [top-undefined #%top])
          (all-from-out "private/operators.rkt"
                        "private/prims.rkt"
                        "private/syntax.rkt"))
 
 (require "private/operators.rkt"
          "private/prims.rkt"
-         "private/syntax.rkt")
+         "private/syntax.rkt"
+         (for-syntax racket/base))
+
+(define-syntax (top-undefined stx)
+  (syntax-case stx ()
+    [(_ . x)
+     (raise-syntax-error 'dssl2
+                         (format "variable ~s is undefined" (syntax-e #'x))
+                         #'x)]))
